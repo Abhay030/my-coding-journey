3adb0b2497a6923ec51dae6ffde33e712ed7089a
Abhay <2022.csai.2213033@gmail.com>
2025-08-13 23:38:15 +0530

    subset sum updated

--- src/dp/subsetSum.java
+++ src/dp/subsetSum.java
@@ -1,29 +1,38 @@
 package dp;
 
 public class subsetSum {
-        static Boolean isSubsetSum(int arr[], int sum) {
-            // code here
-            int n = arr.length;
-            boolean dp[][] = new boolean[n+1][sum+1];
-            return subsetsum(arr , arr.length , sum , dp);
+    public static boolean subsetSumToK(int n, int sum, int arr[]){
+        // Write your code here.
+        boolean dp[][] = new boolean[n+1][sum+1];
+        return subsetsum(arr , arr.length , sum , dp);
+    }
+
+    public static boolean isSubsetSumToKPossible(int n, int k, int arr[]){
+        if(k == 0) return true;
+        if(n == -1) return false;
+
+        if(k >= arr[n]){
+            return isSubsetSumToKPossible(n-1 , k - arr[n] , arr) || isSubsetSumToKPossible(n-1 , k , arr);
+        }
+        else return isSubsetSumToKPossible(n-1 , k , arr);
+    }
+
+    static Boolean subsetsum(int []arr , int n , int sum , boolean dp[][]){
+
+        for(int i = 0; i <= n; i++){
+            dp[i][0] = true;
         }
 
-        static Boolean subsetsum(int []arr , int n , int sum , boolean dp[][]){
-
-            for(int i = 0; i <= n; i++){
-                dp[i][0] = true;
-            }
-
-            for(int i = 1; i<=n; i++){
-                for(int j =1; j<=sum; j++){
-                    if(arr[i-1] <= j){
-                        dp[i][j] = dp[i-1][j - arr[i-1]] || dp[i-1][j];
-                    }
-                    else dp[i][j] = dp[i-1][j];
+        for(int i = 1; i<=n; i++){
+            for(int j =1; j<=sum; j++){
+                if(arr[i-1] <= j){
+                    dp[i][j] = dp[i-1][j - arr[i-1]] || dp[i-1][j];
                 }
+                else dp[i][j] = dp[i-1][j];
             }
-
-            return dp[n][sum];
-
         }
+
+        return dp[n][sum];
+
+    }
 }
