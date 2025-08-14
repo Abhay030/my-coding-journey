4846ff7311ec3c8eaacd9157a22428039090e677
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-08-14 11:56:28 +0530

    Time: 1 ms (72.42%) | Memory: 45.4 MB (97.69%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,10 +1,15 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        // no.of rows
         int m = matrix.length;
+        // no. of columns
         int n = matrix[0].length;
+
+        // to keep recors of the 0 
         int[] row = new int[m];
         int[] column = new int[n];
 
+        // find zero and mark it in the row and colmumn array
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(matrix[i][j] == 0){
@@ -13,6 +18,8 @@ class Solution {
                 }
             }
         }
+
+        // fill the rows and columns wherever we find 0
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
                 if(row[i] == 1 || column[j] == 1){
