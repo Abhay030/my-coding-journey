d83045ddc8dd2e9a52a036e134098585113dc345
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-08-20 22:08:37 +0530

    Time: 1 ms (99.52%) | Memory: 56.9 MB (81.71%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,15 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
-
-        // checking including he current element will increase the overall sum 
-        //  if yes then include otherwise exclude
-        int bestEndingHere = nums[0];
-		int bestSoFar = nums[0];
-		for (int i = 1; i < n; i++) {
-			bestEndingHere = Math.max(nums[i], bestEndingHere + nums[i]);
-			bestSoFar = Math.max(bestSoFar, bestEndingHere);
-		}
-		return bestSoFar; 
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            // Either extend or start fresh
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum;
     }
 }
