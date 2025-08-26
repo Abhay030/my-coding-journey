e63d3522ede0501c63022cdad665299ac7b07baf
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-08-26 11:53:40 +0530

    Time: 4 ms (89.06%) | Memory: 58.3 MB (48.22%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,5 +1,10 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        // return usingHashMap(nums);
+        return usingTwoPointer(nums);
+    }
+
+    private int usingHashMap(int[] nums){
         // Store the numbers in the Map
         HashMap<Integer , Integer> map = new HashMap<>();
         // Tranverse in the Array to check if the number already present.
@@ -8,5 +13,26 @@ class Solution {
             map.put(nums[i] , i);
         }
         return -1;
+    }
+
+    private int usingTwoPointer(int[] nums){
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Detect Cycle
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        while(slow != fast);
+
+        // set slow again to the start
+        slow = nums[0];
+        // iterate to find the cycle
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return fast;
     }
 }
