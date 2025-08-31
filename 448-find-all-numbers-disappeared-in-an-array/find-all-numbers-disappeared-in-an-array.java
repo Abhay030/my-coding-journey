dccd0af4916ecf51d36e4d8d827d1f600aaf9cb1
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-08-31 11:33:51 +0530

    Time: 5 ms (88.64%) | Memory: 55.9 MB (95.55%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,11 +1,14 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        // to store what elements appeared 
         boolean appeared[] = new boolean[nums.length+1];
 
+        // iterate over the array to store their existence
         for(Integer num: nums){
             appeared[num] = true;
         }
 
+        // store not found elements by checking which element's index is false in the appeared array.
         List<Integer> notfoundEle = new ArrayList<>();
         for(int i = 1; i<appeared.length; i++){
             if(!appeared[i]) notfoundEle.add(i);
