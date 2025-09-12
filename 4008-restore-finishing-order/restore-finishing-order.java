713291717d5dab335c8a7d50dbd4a80a2c093c9b
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-09-12 12:52:32 +0530

    Time: 2 ms (69.79%) | Memory: 44.9 MB (81.80%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.java
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -1,5 +1,11 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
+        // return bruteforce(order , friends);
+
+        return optimised(order , friends);
+    }
+    
+    private static int[] bruteforce(int[] order, int[] friends){
         int ans[] = new int[friends.length];
         int index = 0;
         for(int i = 0; i<order.length; i++){
@@ -13,5 +19,28 @@ class Solution {
         }
 
         return ans;
+    }
+
+    private static int[] optimised(int[] order, int[] friends){
+        Set<Integer> participated = new HashSet<Integer>();
+        int size = friends.length;
+        int ans[] = new int[size];
+
+        // store the friends who participated in the race.
+        for(Integer num: friends){
+            participated.add(num);
+        }
+
+        // find their finishing order by linear search
+        int index = 0;
+        for(int n : order){
+            if(participated.contains(n)){
+                ans[index] = n;
+                index++;
+            } 
+        }
+
+        return ans;
+
     }
 }
