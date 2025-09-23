6d015c778d51a575e222f7c7fcd026c3651eb0e2
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-09-23 23:46:03 +0530

    Time: 1 ms (92.88%) | Memory: 44.6 MB (45.95%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,34 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> anslist = new ArrayList<>();
-        backtrack(nums, 0, anslist);
-        return anslist;
+        List<List<Integer>> ansList = new ArrayList<>();
+        getAllPermutation(nums , 0 , ansList);
+        return ansList;
     }
 
-    private void backtrack(int[] nums, int index, List<List<Integer>> anslist) {
-        // Base case: agar index end tak pahunch gaya
-        if (index == nums.length) {
-            List<Integer> temp = new ArrayList<>();
-            for (int num : nums) temp.add(num);
-            anslist.add(temp);
+    public void getAllPermutation(int[] nums , int index , List<List<Integer>> ansList){
+        if(index == nums.length){
+            List<Integer> list = new ArrayList<>();
+            for(int num : nums) list.add(num);
+            ansList.add(list);
             return;
         }
 
-        // Har element ko index position par fix karo
-        for (int i = index; i < nums.length; i++) {
-            swap(nums, index, i);
 
-            // Recursively next index ke liye call
-            backtrack(nums, index + 1, anslist);
-
-            // Backtrack: swap wapas
-            swap(nums, index, i);
+        for(int i = index; i<nums.length; i++){
+            swap(nums , i , index);
+            getAllPermutation(nums , index + 1 , ansList);
+            swap(nums , index , i);
         }
     }
 
-    private void swap(int[] nums, int i, int j) {
+
+    private void swap(int[] nums , int i , int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
+    } 
 }
