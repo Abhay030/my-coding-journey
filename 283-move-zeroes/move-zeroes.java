47a8538d1aabd8ab0674084745dc7bcc3bd65b68
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-10-25 20:40:28 +0530

    Time: 2 ms (79.06%) | Memory: 46.7 MB (7.53%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,6 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        moveZerosBrute(nums);
+        // moveZerosBrute(nums);
+        moveZerosOpti(nums);
     }
     public void moveZerosBrute(int[] nums){
         int lastZero = 0;
@@ -16,6 +17,20 @@ class Solution {
 
         for(int i = n-1; i >= n-count; i--){
             nums[i] = 0;
+        }
+    }
+
+    public void moveZerosOpti(int[] nums){
+        int lastZeroIndex = 0;
+        int n = nums.length;
+
+        for(int i= 0; i<n; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[lastZeroIndex];
+                nums[lastZeroIndex] = temp;
+                lastZeroIndex++;
+            }
         }
     }
 }
