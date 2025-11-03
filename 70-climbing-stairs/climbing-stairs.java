33a0af696d3bc8d089d76a35fc041f83348d5ee9
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-11-03 14:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (6.91%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp , -1);
+        return ways(n , dp);
+    }
+
+    public int ways(int n , int[] dp){
+        // base case 
+        if(n == 0 || n == 1) return 1;
+
+        // return the answer from the dp array if answer is already calculated.
+        if(dp[n] != -1) return dp[n];
+
+        // store the new ans.
+        dp[n] = ways(n - 1 , dp) + ways(n - 2 , dp);
+
+        return dp[n];
+    }
+}
