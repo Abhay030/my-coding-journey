adf0c79160d9a200733d1e27514980b7bb3463a7
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-11-20 10:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (86.51%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean isOneBitCharacter(int[] bits) {
         int n = bits.length;
+
+        // direct cases to prevent loop
         if(n == 1 && ((bits[0]&1) == 0)) return true;
         else if(n >= 2 && ((bits[n-1] & 1) == 0  && (bits[n-2] & 1) == 0)) return true;
         
+        // iterate and if bits[i] == 1 this means its a 2 bit character otherwise single bit.
         int i = 0;
         while(i < n-1){
             if(bits[i] == 1) i += 2;
