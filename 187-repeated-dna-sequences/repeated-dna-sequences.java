c4f630998593623b0b5b7cfb1cef6a42f1490f46
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2025-11-21 20:31:02 +0530

    Time: 25 ms (23.85%) | Memory: 55.8 MB (8.86%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -10,10 +10,12 @@ class Solution {
 
         HashMap<String , Integer> map = new HashMap<>();
 
+        // if length is less than or equal to 10 :- then no DNA is possible as DNA length is 10.
         if(n <= 10){
             return resultList;
         }
 
+        // Step 1:- store the DNA in the Hashmap
         int i = 0, j = 9;
         while(j < n){
             String substr = str.substring(i , j+1);
@@ -22,6 +24,7 @@ class Solution {
             j++;
         }
 
+        // Step 2:- find all the DNA in the map who has 2 or greater than 2 repetition.
         for (String key : map.keySet()) {
             if(map.get(key) >= 2){
                 resultList.add(key);
