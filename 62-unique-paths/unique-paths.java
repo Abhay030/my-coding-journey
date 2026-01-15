f1e21b97bc848d96f889445a8ba70894b7551d14
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2026-01-15 10:34:46 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (70.62%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -2,10 +2,27 @@ class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for(int[] row : dp) Arrays.fill(row , -1);
-        return totalPaths(m-1 , n-1 , dp);
+        // return totalPaths(m-1 , n-1);
+        return totalPaths1(m-1 , n-1 , dp);
     }
 
+    // pure recursion
     private int totalPaths(int row , int column , int[][] dp){
+        if(row == 0 && column == 0){
+            return 1;
+        }
+        if(row < 0 || column < 0){
+            return 0;
+        }
+
+        int up = totalPaths(row - 1 , column , dp);
+        int left = totalPaths(row , column - 1 , dp);
+
+        return up + left;
+    }
+
+    // memoised solution
+    private int totalPaths1(int row , int column , int[][] dp){
         if(row == 0 && column == 0){
             return 1;
         }
@@ -15,8 +32,8 @@ class Solution {
 
         if(dp[row][column] != -1) return dp[row][column];
 
-        int up = totalPaths(row - 1 , column , dp);
-        int left = totalPaths(row , column - 1 , dp);
+        int up = totalPaths1(row - 1 , column , dp);
+        int left = totalPaths1(row , column - 1 , dp);
 
         dp[row][column] =  up + left;
 
