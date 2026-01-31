f41c3626947b95d1bf462c5055cf59971a3dea83
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2026-01-31 13:39:30 +0530

    Time: 31 ms (28.12%) | Memory: 49.2 MB (5.75%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.java
@@ -8,8 +8,50 @@ class Solution {
                 Arrays.fill(arr1d, -1);
             }
         }
-        return solve(0, 0, m - 1, n, m, grid, dp);
+        // return solve(0, 0, m - 1, n, m, grid, dp);
+
+        return solve2(n , m , grid);
 	}
+    
+    public int solve2(int n, int m, int[][] grid) {
+        // 3D DP table
+        int[][][] dp = new int[n][m][m];
+        
+        // Base case: last row
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+                if (j1 == j2) dp[n-1][j1][j2] = grid[n-1][j1];
+                else dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2];
+            }
+        }
+        
+        // Fill DP table bottom-up
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 < m; j1++) {
+                for (int j2 = 0; j2 < m; j2++) {
+                    int maxi = (int)(-1e9);
+                    int curr = (j1 == j2) ? grid[i][j1] 
+                                          : grid[i][j1] + grid[i][j2];
+                    // Try all 9 moves
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                            int newJ1 = j1 + dj1;
+                            int newJ2 = j2 + dj2;
+                            if (newJ1 >= 0 && newJ1 < m &&
+                                newJ2 >= 0 && newJ2 < m) {
+                                maxi = Math.max(maxi, curr + 
+                                       dp[i+1][newJ1][newJ2]);
+                            } else {
+                                maxi = Math.max(maxi, (int)(-1e9));
+                            }
+                        }
+                    }
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][m-1];
+    }
 
 	private static int solve(int i , int j1 , int j2 , int n , int m , int[][] grid , int[][][] dp){
 		// boundary check 
