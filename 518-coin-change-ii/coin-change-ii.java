9dd23948dd3ece66271703486f0e2c8b95665ea9
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2026-02-17 11:55:09 +0530

    Time: 16 ms (37.73%) | Memory: 76.9 MB (5.03%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -6,9 +6,14 @@ class Solution {
         for(int[] arr: dp){
             Arrays.fill(arr , -1);
         }
-        return solve(n-1 , amount , coins , dp);
+        // return solve(n-1 , amount , coins , dp);
+
+        return solve2(coins , amount);
+
+
     }
 
+    // memosied solution
     private int solve(int n , int amount , int[] coins , int[][] dp){
         if(n == 0){
             if(amount % coins[n] == 0) return 1;
@@ -24,5 +29,29 @@ class Solution {
         }
 
         return dp[n][amount] = take + notTake;
+    }
+
+    private int solve2(int[] coins , int amount){
+        int n = coins.length;
+
+        int[][] dp = new int[n][amount+1];
+
+        for(int t = 0; t <= amount; t++){
+            if(t % coins[0] == 0) dp[0][t] = 1;
+            else dp[0][t] = 0;
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int t = 0; t <= amount; t++){
+                int notTake = dp[i-1][t];
+                int take = 0;
+                if(t >= coins[i]){
+                    take = dp[i][t-coins[i]];
+                }
+                dp[i][t] = take + notTake;
+            }
+        }
+
+        return dp[n-1][amount];
     }
 }
