17ec3c233b9fe505eda5ce6d5d2dc00eecae774c
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2026-03-11 19:11:35 +0530

    Time: 1 ms (43.74%) | Memory: 61.2 MB (84.12%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,5 +1,16 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        // bruteforce(nums , k);
+
+        // optimised
+        int n = nums.length;
+        k = k % n;
+        reverse(nums , 0 , n-1);
+        reverse(nums , 0 , k-1);
+        reverse(nums , k , n-1);
+    }
+
+    private void bruteforce(int[] nums , int k){
         int n = nums.length;
         k = k % n; 
         if (k == 0) return;
@@ -20,4 +31,16 @@ class Solution {
             nums[i] = temp[i];
         }
     }
+
+    private void reverse(int[] nums ,int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
 }
