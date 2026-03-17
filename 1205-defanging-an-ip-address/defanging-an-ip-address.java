f4def843fa5a7d26c251ceb695a12dc8622cc0dd
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2026-03-18 01:29:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (87.55%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -1,14 +1,16 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String[] addr = address.split("\\.");
-        int n = addr.length;
+        // String[] addr = address.split("\\.");
+        // int n = addr.length;
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < addr.length-1; i++){
-                sb.append(addr[i]);
-                sb.append("[.]");
-        }
-        if(n > 0) sb.append(addr[addr.length-1]);
-        return sb.toString();
+        // StringBuilder sb = new StringBuilder();
+        // for(int i = 0; i < addr.length-1; i++){
+        //         sb.append(addr[i]);
+        //         sb.append("[.]");
+        // }
+        // if(n > 0) sb.append(addr[addr.length-1]);
+        // return sb.toString();
+
+        return address.replace("." , "[.]");
     }
 }
