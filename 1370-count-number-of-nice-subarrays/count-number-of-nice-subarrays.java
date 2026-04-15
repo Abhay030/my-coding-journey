fc556b9df7702f1629949c5291d045537f9fca4f
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2026-04-15 12:59:22 +0530

    Time: 14 ms (49.62%) | Memory: 59.6 MB (43.72%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -4,22 +4,21 @@ class Solution {
     }
 
     public int solve(int[] nums , int k){
-        int left=0;
-        int oddCount=0;
-        int count=0;
+        int left = 0;
+        int oddCount = 0;
+        int nice = 0;
 
-        for(int right=0; right<nums.length; right++){
+        for(int i = 0; i < nums.length; i++){
+            int num = nums[i];
+            if(num % 2 != 0) oddCount++;
 
-            if(nums[right]%2!=0) oddCount++;
-
-            while(oddCount>k){
-                if(nums[left]%2!=0) oddCount--;
+            while(oddCount > k) {
+                if(nums[left] %2 != 0) oddCount--;
                 left++;
             }
-
-            count+=(right-left+1);
+            nice += i-left+1;
         }
 
-        return count;
+        return nice;
     }
 }
