9148ea4b67b8d4cf1da82c8e270ae1ec8ba9d3bd
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2026-05-17 22:56:25 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (85.57%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.java
+++ 113-path-sum-ii/path-sum-ii.java
@@ -23,14 +23,19 @@ class Solution {
 
     private void findPath(TreeNode root , int targetSum , List<Integer> myPath){
         if(root == null) return;
+        //Step 1 :- add
         myPath.add(root.val);
 
+        //Step 2:- Check if the condition is met then add the path
         if(root.left == null && root.right == null && root.val == targetSum){
             allPath.add(new ArrayList<>(myPath));
         }
 
+        // Step 3:- Traverse left and right to find the path
         findPath(root.left , targetSum - root.val , myPath);
         findPath(root.right , targetSum - root.val , myPath);
+
+        // Step 4:- backtrack or remove as we move out of the recursive calls
         myPath.remove(myPath.size()-1);
 
     }
