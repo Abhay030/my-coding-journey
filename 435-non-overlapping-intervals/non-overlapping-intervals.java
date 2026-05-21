11007641914a40e4754d5674c07e75086274bd6e
Abhay Awasthi <154865994+Abhay030@users.noreply.github.com>
2026-05-21 13:54:06 +0530

    Time: 45 ms (83.89%) | Memory: 115.6 MB (87.22%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int eraseOverlapIntervals(int[][] points) {
-        Arrays.sort(points , (a , b) -> Integer.compare(a[1], b[1]));
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals , (a,b) -> a[1] - b[1]);
 
-        int lastEndTime = points[0][1];
+        int lastEndTime = intervals[0][1];
         int count = 0;
 
-        for(int i = 1; i<points.length; i++){
-            if(points[i][0] < lastEndTime){
+        for(int i = 1; i<intervals.length; i++){
+
+            if(lastEndTime > intervals[i][0]){
                 count++;
             }
-            else{
-                lastEndTime = points[i][1];
+            else {
+                lastEndTime = intervals[i][1]; 
             }
         }
-
         return count;
     }
 }
